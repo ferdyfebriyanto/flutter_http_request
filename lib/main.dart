4b116beb26449136df5e9c2e89f9119491d9deba
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2022-09-07 22:49:30 +0700

    Menambahkan movie_list.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_http_request/pages/movie_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MovieList();
   }
 }
---
+++ lib/pages/movie_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieList extends StatefulWidget {
+  const MovieList({Key? key}) : super(key: key);
+
+  @override
+  State<MovieList> createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
